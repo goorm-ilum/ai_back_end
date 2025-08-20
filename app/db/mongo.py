5143088b70a53340449dfb8de63f6eba9c75d3f4
조유경 <yugyeongjo1215@gmail.com>
2025-08-20 16:23:16 +0900

    add : mongo collection name

--- app/db/mongo.py
+++ app/db/mongo.py
@@ -2,7 +2,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from app.config.settings import MONGODB_URL
 
 mongo_client = AsyncIOMotorClient(MONGODB_URL)
-mongo_db = mongo_client["my_mongo_db"]
+mongo_db = mongo_client["TalkTrip"]
 
 def get_mongo_db():
     return mongo_db
