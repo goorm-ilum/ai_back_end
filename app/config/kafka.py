41e96d5108199cd427a4bea8395be2580974f3f4
조유경 <yugyeongjo1215@gmail.com>
2025-08-25 17:23:02 +0900

    add : kafka cluster

--- app/config/kafka.py
+++ app/config/kafka.py
@@ -1,12 +1,16 @@
 from aiokafka import AIOKafkaConsumer
 import json
 
-KAFKA_BROKER = "172.16.24.72:9092"
+KAFKA_BROKERS = [
+    "172.16.24.72:19092",
+    "172.16.24.72:29092",
+    "172.16.24.72:39092"
+]
 
 # Consumer 설정
 consumer = AIOKafkaConsumer(
     'review.raw',
-    bootstrap_servers="172.16.24.72:9092",
+    bootstrap_servers=KAFKA_BROKERS,
     group_id="fastapi-ai-group",
     value_deserializer=lambda m: json.loads(m.decode('utf-8'))
 )
